c987e75c697c9f436f5a65580dd826af920581e9
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-08-02 16:01:00 -0500

    style updates to homepage

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ export default function App() {
             <div className='flex flex-col justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2'>
                 <h1 className='text-6xl md:text-8xl font-black tracking-tight text-center leading-none'>
                     <span className='whitespace-nowrap block uppercase bg-clip-text text-transparent bg-gradient-to-r from-lime-400 via-teal-400 to-rose-400'>Viva</span>
-                    <span className='whitespace-nowrap block dark:text-white text-gray-900'>Web Design</span>
+                    <span className='whitespace-nowrap block text-white'>Web Design</span>
                 </h1>
             </div>
         </main>
-        <nav className='border border-black dark:border-rose-400 dark:bg-gray-900 sticky top-0'>
+        <nav className='border border-rose-400 bg-gray-900 sticky top-0'>
             <div className='py-3 px-10 mx-auto max-w-screen-xl md:px-14'>
                 <div className='flex justify-center md:justify-evenly overflow-hidden '>
                     <ul className='flex flex-row space-x-6 items-center content-center text-sm md:text-lg font-medium'>
@@ -37,7 +37,7 @@ export default function App() {
 
         <main className='bg-white xl:border border-black'>
             <div className='max-w-2xl mx-auto py-6 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8'>
-                <h2 className='text-3xl text-center tracking-tight font-bold text-gray-900 md:text-6xl'>
+                <h2 className='text-3xl text-center tracking-tight font-light text-gray-900 md:text-6xl'>
                     <span className='font-light text-emerald-400'>VIVA</span> Discover
                 </h2>
                 <p className='text-lg text-center tracking-tight text-gray-900 mb-6 xl:mb-10'>
@@ -45,7 +45,7 @@ export default function App() {
                 </p>
                 <div className='grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8 items-center'>
                     <Link to='/design' className='group'>
-                        <div className='w-full rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 bg-gradient-to-l from-lime-400 via-teal-400 to-rose-400'>
+                        <div className='w-full rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 bg-gradient-to-t from-purple-400 via-emerald-300 to-emerald-500'>
                             <h3 className='mt-1 ml-4 text-2xl text-center xl:text-start font-light text-gray-900'>Hello World</h3>
                             <div className='xl:h-96 mb-10 xl:mb-16 object-center object-cover group-hover:opacity-75'>  
                                 <Player
@@ -60,7 +60,7 @@ export default function App() {
                         </div>
                     </Link>
                     <Link to='/design' className='group'>
-                        <div className='w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 bg-gradient-to-r from-lime-400 via-teal-400 to-rose-400'>
+                        <div className='w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 bg-gradient-to-t from-cyan-400 via-rose-300 to-rose-500'>
                             <h3 className='mt-1 ml-4 text-2xl text-center xl:text-start font-light text-gray-900'>Hello Animation</h3>
                             <div className='xl:h-96 mb-10 xl:mb-16 object-center object-cover group-hover:opacity-75'>  
                                 <Player
@@ -75,7 +75,7 @@ export default function App() {
                         </div>
                     </Link>
                     <Link to='/design' className='group'>
-                        <div className='w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 bg-gradient-to-l from-lime-400 via-teal-400 to-rose-400'>
+                        <div className='w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 bg-gradient-to-t from-lime-400 via-yellow-300 to-yellow-500'>
                             <h3 className='mt-1 ml-4 text-2xl text-center xl:text-start font-light text-gray-900'>Hello Data</h3>
                             <div className='xl:h-96 mb-10 xl:mb-16 object-center object-cover group-hover:opacity-75'>  
                                 <Player
@@ -95,38 +95,38 @@ export default function App() {
         <main className="grid grid-cols-1 border border-black">
             <div className="bg-black px-4 py-10">
                 <h1 className="text-2xl text-center md:text-4xl bg-clip-text text-transparent bg-gradient-to-r from-lime-400 via-teal-400 to-rose-400">
-                    Your Idea Starts Here
+                    Your Ideas Start Here
                 </h1>
             </div>
         </main>
         <main className='flex flex-col xl:flex-row h-screen'>
             <div className='w-full xl:w-1/2 h-screen flex items-center justify-center bg-sky-400 xl:border-r xl:border-b border-black'>
                 <div className='w-4/6'>
-                    <h2 className='mt-2 md:mt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-start tracking-tight font-bold text-gray-900'>
+                    <h2 className='pt-2 md:pt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-start tracking-tight font-bold text-gray-900'>
                         You have great
                         <span className='text-lime-400'> ideas</span>
                     </h2>
-                    <p className='mt-1 md:mt-2 xl:mt-3 text-lg md:text-2xl xl:text-4xl text-start tracking-tight font-light text-white'>
+                    <p className='pt-1 md:pt-2 xl:pt-3 text-lg md:text-2xl xl:text-4xl text-start tracking-tight font-light text-white'>
                         We want you to <span className='text-lime-300'> try them</span>, lots of them,
                         and find out what works.
                     </p>
-                    <h2 className='mt-2 md:mt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-start tracking-tight font-bold text-gray-900'>
+                    <h2 className='pt-3 md:pt-4 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-start tracking-tight font-bold text-gray-900'>
                         Create your own road 
                     </h2>
-                    <p className='mt-1 md:mt-2 xl:mt-3 text-lg md:text-2xl xl:text-4xl text-start tracking-tight font-light text-white'>
+                    <p className='pt-1 md:pt-2 xl:pt-3 text-lg md:text-2xl xl:text-4xl text-start tracking-tight font-light text-white'>
                         <span className='text-lime-300'>Independent</span> doesn't mean alone
                     </p>
-                    <h2 className='mt-2 md:mt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-start tracking-tight font-bold text-gray-900'>
+                    <h2 className='pt-3 md:pt-4 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-start tracking-tight font-bold text-gray-900'>
                         Your site, Your way
                     </h2>
-                    <p className='mt-3 md:mt-5 xl:mt-6 text-lg md:text-2xl xl:text-4xl text-start tracking-tight font-light text-white'>
+                    <p className='pt-1 md:pt-2 xl:pt-3 text-lg md:text-2xl xl:text-4xl text-start tracking-tight font-light text-white'>
                         We just provide the tools for your idea to <span className='text-lime-300'>grow</span>.
                     </p>
                 </div>
             </div>
             <div className='w-full xl:w-1/2 h-screen flex items-center justify-center bg-purple-400 xl:border-l xl:border-b border-black'>
                 <div className='w-4/6'>
-                    <h1 className="text-3xl text-center tracking-tight font-bold text-gray-900 dark:text-white md:text-6xl">
+                    <h1 className="text-3xl text-center tracking-tight font-light text-gray-900 dark:text-white md:text-6xl">
                         <span className='font-light text-sky-400'>VIVA</span> Design</h1>
                     <p className="text-lg text-center tracking-tight text-gray-900 dark:text-white">
                         Your ideas brought to life
@@ -152,7 +152,7 @@ export default function App() {
                     </p>
                     <Player
                         src='https://assets8.lottiefiles.com/packages/lf20_1oucccvj.json'
-                        className='pt-1 xl:pt-3'
+                        className='pt-1 xl:pt-1'
                         background='transparent'
                         loop={true}
                         autoplay={true}
@@ -162,29 +162,29 @@ export default function App() {
             </div>
             <div className='w-full xl:w-1/2 h-screen flex items-center justify-center bg-rose-500 xl:border-l xl:border-b border-black'>
             <div className='w-4/6'>
-                    <h2 className='mt-2 md:mt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-start tracking-tight font-light text-sky-400'>
+                    <h2 className='pt-2 md:pt-3 xl:pt-4 text-2xl md:text-4xl xl:text-6xl text-center xl:text-end tracking-tight font-light text-sky-400'>
                         VIVA➕
                     </h2>
-                    <p className='mt-1 md:mt-2 xl:mt-3 text-lg md:text-2xl xl:text-4xl text-start tracking-tight font-light text-white'>
-                        - Built into every design
+                    <p className='pt-1 md:pt-2 xl:pt-3 text-lg md:text-2xl xl:text-4xl text-center xl:text-end tracking-tight font-light text-white'>
+                        Built into every design
                     </p>
-                    <h2 className='mt-2 md:mt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-start tracking-tight font-light text-gray-900'>
+                    <h2 className='pt-2 md:pt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-center xl:text-end tracking-tight font-light text-gray-900'>
                         Hello<span className='text-sky-400'> World</span>
                     </h2>
-                    <p className='mt-1 md:mt-2 xl:mt-3 text-lg md:text-2xl xl:text-4xl text-start tracking-tight font-light text-white'>
-                        - Your idea brought to life.
+                    <p className='pt-1 md:pt-2 xl:pt-3 text-lg md:text-2xl xl:text-4xl text-center xl:text-end tracking-tight font-light text-white'>
+                        Your idea brought to life.
                     </p>
-                    <h2 className='mt-2 md:mt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-start tracking-tight font-light text-gray-900'>
+                    <h2 className='pt-2 md:pt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-center xl:text-end tracking-tight font-light text-gray-900'>
                         Hello<span className='text-sky-400'> Animation</span>
                     </h2>
-                    <p className='mt-1 md:mt-2 xl:mt-3 text-lg md:text-2xl xl:text-4xl text-start tracking-tight font-light text-white'>
-                        - Motion with a purpose.
+                    <p className='pt-1 md:pt-2 xl:pt-3 text-lg md:text-2xl xl:text-4xl text-center xl:text-end tracking-tight font-light text-white'>
+                        Motion with a purpose.
                     </p>
-                    <h2 className='mt-2 md:mt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-start tracking-tight font-light text-gray-900'>
+                    <h2 className='pt-2 md:pt-3 xl:pt-10 text-2xl md:text-4xl xl:text-6xl text-center xl:text-end tracking-tight font-light text-gray-900'>
                         Hello<span className='text-sky-400'> Data</span>
                     </h2>
-                    <p className='mt-3 md:mt-5 xl:mt-6 text-lg md:text-2xl xl:text-4xl text-start tracking-tight font-light text-white'>
-                        - Your idea runs on data. 
+                    <p className='pt-3 md:pt-5 xl:pt-6 text-lg md:text-2xl xl:text-4xl text-center xl:text-end tracking-tight font-light text-white'>
+                        Your idea runs on data. 
                     </p>
                 </div>
             </div>
@@ -192,7 +192,7 @@ export default function App() {
         <main className="grid grid-cols-1 border border-black">
             <div className="bg-black px-4 py-10">
                 <h1 className="text-2xl text-center md:text-4xl bg-clip-text text-transparent bg-gradient-to-r from-lime-400 via-teal-400 to-rose-400">
-                    Your Idea With Emotion
+                    Your Ideas With Emotion
                 </h1>
             </div>
         </main>
@@ -220,11 +220,11 @@ export default function App() {
         <footer className='flex flex-col xl:flex-row h-96 xl:h-screen'>
             <div className='w-full h-screen flex flex-col items-center justify-center bg-gray-900 xl:border border-black'>
                 <div className='w-4/6'>
-                    <h1 className='p-4 text-4xl md:text-6xl font-black tracking-tight text-center lg:text-start leading-none'>
+                    <h1 className='pt-2 text-4xl md:text-6xl font-black tracking-tight text-center lg:text-start leading-none'>
                         <span className='whitespace-nowrap block text-rose-400 uppercase'>Viva</span>
                         <span className='whitespace-nowrap block dark:text-white text-gray-900'>Web Design</span>
                     </h1>
-                    <div className='pt-4 md:pt-6 flex flex-col content-center'>
+                    <div className='pt-4 md:pt-20 flex flex-col content-center'>
                         <h2 className='items-center text-center text-4xl font-bold tracking-tight md:text-6xl md:pl-1 lg:pb-4'>
                             <span className="block text-teal-300 ">Ready to VIVA</span>
                             <span className="block text-white lg:pt-2">Let's get started today.</span>
