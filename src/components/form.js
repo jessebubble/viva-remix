f3bb53d6966769087776befb7f1724e86a7bb5e9
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-10-03 15:42:53 -0500

    emailjs added to form

--- src/components/form.js
+++ src/components/form.js
@@ -1,9 +1,23 @@
 import { Link } from 'react-router-dom'
 import { FiInstagram, FiTwitter, FiLinkedin, FiMail } from "react-icons/fi";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Form() {
-    return (
+    const form = useRef();
 
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_hxrxpdb', 'template_p9czz9g', form.current, 'Vg9DbywednMl26hQU')
+            .then((result) => {
+                    console.log(result.text);
+                },(error) => {
+                    console.log(error.text);
+                });
+                e.target.reset();
+    };
+    return (
         <>
         <main className="relative bg-slate-900 p-2 md:p-6 lg:p-10">
             <div className="absolute inset-0">
@@ -61,21 +75,39 @@ export default function Form() {
                 </div>
                 <div className="bg-slate-900 py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12 -mt-36 md:-mt-0">
                     <div className="mx-auto max-w-lg lg:max-w-none">
-                        <form action="#" method="POST" className="grid grid-cols-1 gap-y-6">
+                        <form ref={form} onSubmit={sendEmail} action="#" method="POST" className="grid grid-cols-1 gap-y-6">
                             <div>
-                                <label for="full-name" className="sr-only">Full name</label>
-                                <input type="text" name="full-name" id="full-name" autocomplete="name" className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" placeholder="Full name"/>
+                                <label htmlFor="name" className="sr-only">Full name</label>
+                                <input 
+                                    type="text" 
+                                    name="name" 
+                                    id="name" 
+                                    autoComplete="name" 
+                                    className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" placeholder="Full name"
+                                />
                             </div>
                             <div>
-                                <label for="email" className="sr-only">Email</label>
-                                <input id="email" name="email" type="email" autocomplete="email" className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" placeholder="Email"/>
+                                <label htmlFor="email" className="sr-only">Email</label>
+                                <input 
+                                    id="email" 
+                                    name="email" 
+                                    type="email" 
+                                    autoComplete="email" 
+                                    className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" placeholder="Email"
+                                />
                             </div>
                             <div>
-                                <label for="phone" className="sr-only">Phone</label>
-                                <input type="text" name="phone" id="phone" autocomplete="tel" className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" placeholder="Phone"/>
+                                <label htmlFor="phone" className="sr-only">Phone</label>
+                                <input 
+                                    type="text" 
+                                    name="phone" 
+                                    id="phone" 
+                                    autoComplete="tel" 
+                                    className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" placeholder="Phone"
+                                />
                             </div>
                             <div>
-                                <label for="message" className="sr-only">Message</label>
+                                <label htmlFor="message" className="sr-only">Message</label>
                                 <textarea id="message" name="message" rows="4" className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" placeholder="Message"></textarea>
                             </div>
                             <div>
