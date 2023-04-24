c5076509bb0800c9cce6ad48a54b7538d8591b2c
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2023-04-24 13:16:12 -0500

    frontend code moved into remix folder

--- app/components/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Facebook } from "./Facebook";
-export { default as Instagram } from "./Instagram";
-export { default as Linkedin } from "./Linkedin";
-export { default as Mail } from "./Mail";
--- app/lib/sanity.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const client = createClient({ 
-    projectId: "vk33ybcu",
-    dataset: "production",
-    apiVersion: "2023-04-17",
-    useCdn: true, 
-    //token: process.env.SANITY_SECRET_TOKEN 
-});
