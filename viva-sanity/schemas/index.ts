eb7cc2978e912ffae1913e0b022870d62b92406b
jessebubble <jesseovr@gmail.com>
2023-11-06 13:20:54 -0600

    removed paths not used, website restyle coming

--- viva-sanity/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import { collectionCommunity } from './collectionCommunity'
-import { collectionTwitchin } from './collectionTwitchin'
-import { collectionDeveloper } from './collectionDeveloper'
-
-export const schemaTypes = [collectionCommunity, collectionTwitchin, collectionDeveloper]
